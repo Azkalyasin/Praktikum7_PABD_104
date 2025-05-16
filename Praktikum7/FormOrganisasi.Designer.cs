0347a822d3e2852d37b1ee16ebdbf647b7b1b364
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-05-16 20:49:09 +0700

    add FormOrganisasi

---
+++ Praktikum7/FormOrganisasi.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Praktikum7
+{
+    partial class FormOrganisasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridViewOrganisasi = new System.Windows.Forms.DataGridView();
+            this.txtDeskripsi = new System.Windows.Forms.TextBox();
+            this.txtTahunBerdiri = new System.Windows.Forms.TextBox();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.txtKeterangan = new System.Windows.Forms.TextBox();
+            this.txtNamaOrganisasi = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrganisasi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama Organisasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tahun Berdiri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jumlah Dana";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Keterangan";
+            // 
+            // dataGridViewOrganisasi
+            // 
+            this.dataGridViewOrganisasi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrganisasi.Location = new System.Drawing.Point(131, 371);
+            this.dataGridViewOrganisasi.Name = "dataGridViewOrganisasi";
+            this.dataGridViewOrganisasi.RowHeadersWidth = 51;
+            this.dataGridViewOrganisasi.RowTemplate.Height = 24;
+            this.dataGridViewOrganisasi.Size = new System.Drawing.Size(744, 328);
+            this.dataGridViewOrganisasi.TabIndex = 5;
+            this.dataGridViewOrganisasi.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOrganisasi_CellClick);
+            // 
+            // txtDeskripsi
+            // 
+            this.txtDeskripsi.Location = new System.Drawing.Point(272, 117);
+            this.txtDeskripsi.Name = "txtDeskripsi";
+            this.txtDeskripsi.Size = new System.Drawing.Size(400, 22);
+            this.txtDeskripsi.TabIndex = 6;
+            this.txtDeskripsi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTahunBerdiri
+            // 
+            this.txtTahunBerdiri.Location = new System.Drawing.Point(272, 184);
+            this.txtTahunBerdiri.Name = "txtTahunBerdiri";
+            this.txtTahunBerdiri.Size = new System.Drawing.Size(400, 22);
+            this.txtTahunBerdiri.TabIndex = 7;
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(272, 242);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(400, 22);
+            this.txtJumlah.TabIndex = 8;
+            // 
+            // txtKeterangan
+            // 
+            this.txtKeterangan.Location = new System.Drawing.Point(272, 303);
+            this.txtKeterangan.Name = "txtKeterangan";
+            this.txtKeterangan.Size = new System.Drawing.Size(400, 22);
+            this.txtKeterangan.TabIndex = 9;
+            // 
+            // txtNamaOrganisasi
+            // 
+            this.txtNamaOrganisasi.Location = new System.Drawing.Point(272, 45);
+            this.txtNamaOrganisasi.Name = "txtNamaOrganisasi";
+            this.txtNamaOrganisasi.Size = new System.Drawing.Size(400, 22);
+            this.txtNamaOrganisasi.TabIndex = 10;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(127, 716);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(64, 16);
+            this.lblMessage.TabIndex = 11;
+            this.lblMessage.Text = "Massage";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Deskripsi";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackgroundImage = global::Praktikum7.Properties.Resources.refrese;
+            this.btnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRefresh.Location = new System.Drawing.Point(800, 273);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(100, 53);
+            this.btnRefresh.TabIndex = 15;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImage = global::Praktikum7.Properties.Resources.Delete;
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDelete.Location = new System.Drawing.Point(800, 197);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 48);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackgroundImage = global::Praktikum7.Properties.Resources.update;
+            this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUpdate.Location = new System.Drawing.Point(800, 123);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 52);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnInsert.BackgroundImage = global::Praktikum7.Properties.Resources.insert;
+            this.btnInsert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInsert.Location = new System.Drawing.Point(800, 51);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(100, 61);
+            this.btnInsert.TabIndex = 12;
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // FormOrganisasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 741);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtNamaOrganisasi);
+            this.Controls.Add(this.txtKeterangan);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.txtTahunBerdiri);
+            this.Controls.Add(this.txtDeskripsi);
+            this.Controls.Add(this.dataGridViewOrganisasi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormOrganisasi";
+            this.Text = "FormOrganisasi";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrganisasi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridViewOrganisasi;
+        private System.Windows.Forms.TextBox txtDeskripsi;
+        private System.Windows.Forms.TextBox txtTahunBerdiri;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.TextBox txtKeterangan;
+        private System.Windows.Forms.TextBox txtNamaOrganisasi;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Praktikum7/FormOrganisasi.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum7
+{
+    public partial class FormOrganisasi: Form
+    {
+        static string connectionString = "Data Source=LAPTOP-PGU1KG1D\\AZKALADZKIA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True;";
+        private int selectedIdOrganisasi = -1;
+
+        public FormOrganisasi()
+        {
+            InitializeComponent();
+            this.BackColor = Color.MediumPurple;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string namaOrganisasi = txtNamaOrganisasi.Text;
+            string deskripsi = txtDeskripsi.Text;
+            int tahunBerdiri;
+            decimal jumlah;
+            string keterangan = txtKeterangan.Text;
+
+            // Validasi input
+            if (string.IsNullOrEmpty(namaOrganisasi) || string.IsNullOrEmpty(deskripsi) ||
+                !int.TryParse(txtTahunBerdiri.Text, out tahunBerdiri) ||
+                !decimal.TryParse(txtJumlah.Text, out jumlah) || string.IsNullOrEmpty(keterangan))
+            {
+                lblMessage.Text = "Isi kolom dengan data yang sesuai";
+                return;
+            }
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlTransaction transaction = null;
+
+                try
+                {
+                    conn.Open();
+                    transaction = conn.BeginTransaction();
+
+                    SqlCommand cmd = new SqlCommand
+                    {
+                        Connection = conn,
+                        Transaction = transaction
+                    };
+
+                    // Insert into Organisasi table
+                    cmd.CommandText = "INSERT INTO Organisasi (NamaOrganisasi, Deskripsi, TahunBerdiri) OUTPUT INSERTED.ID_Organisasi VALUES (@NamaOrganisasi, @Deskripsi, @TahunBerdiri)";
+                    cmd.Parameters.AddWithValue("@NamaOrganisasi", namaOrganisasi);
+                    cmd.Parameters.AddWithValue("@Deskripsi", deskripsi);
+                    cmd.Parameters.AddWithValue("@TahunBerdiri", tahunBerdiri);
+
+                    object result = cmd.ExecuteScalar();
+                    if (result == DBNull.Value)
+                    {
+                        lblMessage.Text = "Gagal mengambil ID Organisasi";
+                        return;
+                    }
+
+                    int idOrganisasi = Convert.ToInt32(result);
+
+                    // Insert into Keuangan table
+                    cmd.CommandText = "INSERT INTO Keuangan (ID_Organisasi, Jenis, Jumlah, Tanggal, Keterangan) VALUES (@ID_Organisasi, 'Pemasukan', @Jumlah, GETDATE(), @Keterangan)";
+                    cmd.Parameters.AddWithValue("@ID_Organisasi", idOrganisasi);
+                    cmd.Parameters.AddWithValue("@Jumlah", jumlah);
+                    cmd.Parameters.AddWithValue("@Keterangan", keterangan);
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    lblMessage.Text = "Data berhasil disimpan";
+                    LoadJoinedData(); // Reload data after insert
+                }
+                catch (Exception ex)
+                {
+                    transaction?.Rollback();
+                    lblMessage.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+        private void LoadJoinedData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+        SELECT 
+            Organisasi.ID_Organisasi,
+            Organisasi.NamaOrganisasi,
+            Organisasi.Deskripsi,
+            Organisasi.TahunBerdiri,
+            Keuangan.Jumlah,
+            Keuangan.Tanggal,
+            Keuangan.Keterangan
+        FROM 
+            Organisasi
+        INNER JOIN 
+            Keuangan ON Organisasi.ID_Organisasi = Keuangan.ID_Organisasi";
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    dataAdapter.Fill(dataTable);
+                    dataGridViewOrganisasi.DataSource = dataTable; // Menampilkan hasil gabungan di DataGridView
+
+                    // Menyembunyikan kolom ID_Organisasi dari tampilan DataGridView
+                    dataGridViewOrganisasi.Columns["ID_Organisasi"].Visible = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridViewOrganisasi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Pastikan indeks baris valid
+            if (e.RowIndex >= 0 && dataGridViewOrganisasi.Rows.Count > 0)
+            {
+                try
+                {
+                    // Ambil ID_Organisasi dari baris yang dipilih
+                    var idOrganisasiCellValue = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["ID_Organisasi"].Value;
+                    if (idOrganisasiCellValue != null && idOrganisasiCellValue != DBNull.Value)
+                    {
+                        // Simpan ID yang dipilih untuk digunakan nanti (pada operasi update/delete)
+                        selectedIdOrganisasi = Convert.ToInt32(idOrganisasiCellValue);
+
+                        // Isi TextBox dengan data dari baris yang dipilih
+                        txtNamaOrganisasi.Text = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["NamaOrganisasi"].Value?.ToString() ?? "";
+                        txtDeskripsi.Text = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["Deskripsi"].Value?.ToString() ?? "";
+                        txtTahunBerdiri.Text = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["TahunBerdiri"].Value?.ToString() ?? "";
+
+                        // Mengambil nilai dari kolom "Jumlah"
+                        var jumlahValue = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["Jumlah"].Value;
+
+                        // Cek apakah jumlahValue tidak null dan bukan DBNull
+                        if (jumlahValue != null && jumlahValue != DBNull.Value)
+                        {
+                            // Jika ada nilai yang valid, tampilkan
+                            txtJumlah.Text = jumlahValue.ToString();
+                        }
+                        else
+                        {
+                            // Set text box ke kosong jika null atau DBNull
+                            txtJumlah.Text = "";
+                        }
+
+                        // Cek dan isi keterangan
+                        txtKeterangan.Text = dataGridViewOrganisasi.Rows[e.RowIndex].Cells["Keterangan"].Value?.ToString() ?? "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("ID organisasi tidak valid.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saat mengambil data: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedIdOrganisasi == -1)
+            {
+                lblMessage.Text = "Pilih nama organisasi yang ingin dihapus";
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlTransaction transaction = null;
+
+                try
+                {
+                    conn.Open();
+                    transaction = conn.BeginTransaction();
+
+                    SqlCommand cmd = new SqlCommand
+                    {
+                        Connection = conn,
+                        Transaction = transaction
+                    };
+
+                    // Delete from Keuangan table
+                    cmd.CommandText = "DELETE FROM Keuangan WHERE ID_Organisasi = @ID_Organisasi";
+                    cmd.Parameters.AddWithValue("@ID_Organisasi", selectedIdOrganisasi);
+                    cmd.ExecuteNonQuery();
+
+                    // Delete from Organisasi table
+                    cmd.CommandText = "DELETE FROM Organisasi WHERE ID_Organisasi = @ID_Organisasi";
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    lblMessage.Text = "Data berhasil dihapus.";
+                    LoadJoinedData(); // Reload DataGridView after delete
+                }
+                catch (Exception ex)
+                {
+                    transaction?.Rollback();
+                    lblMessage.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedIdOrganisasi == -1)
+            {
+                lblMessage.Text = "Pilih nama organisasi yang ingin diubah";
+                return;
+            }
+
+            // Ambil data dari textboxs untuk update
+            string namaOrganisasi = txtNamaOrganisasi.Text;
+            string deskripsi = txtDeskripsi.Text;
+            int tahunBerdiri;
+            decimal jumlah;
+            string keterangan = txtKeterangan.Text;
+
+            // Validasi input
+            if (string.IsNullOrEmpty(namaOrganisasi) || string.IsNullOrEmpty(deskripsi) ||
+                !int.TryParse(txtTahunBerdiri.Text, out tahunBerdiri) ||
+                !decimal.TryParse(txtJumlah.Text, out jumlah) || string.IsNullOrEmpty(keterangan))
+            {
+                lblMessage.Text = "Isi kolom dengan data yang sesuai!";
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlTransaction transaction = null;
+
+                try
+                {
+                    conn.Open();
+                    transaction = conn.BeginTransaction();
+
+                    SqlCommand cmd = new SqlCommand
+                    {
+                        Connection = conn,
+                        Transaction = transaction
+                    };
+
+                    cmd.CommandText = "UPDATE Organisasi SET NamaOrganisasi = @NamaOrganisasi, Deskripsi = @Deskripsi, TahunBerdiri = @TahunBerdiri WHERE ID_Organisasi = @ID_Organisasi";
+                    cmd.Parameters.AddWithValue("@NamaOrganisasi", namaOrganisasi);
+                    cmd.Parameters.AddWithValue("@Deskripsi", deskripsi);
+                    cmd.Parameters.AddWithValue("@TahunBerdiri", tahunBerdiri);
+                    cmd.Parameters.AddWithValue("@ID_Organisasi", selectedIdOrganisasi);
+                    cmd.ExecuteNonQuery();
+
+                    // Update Keuangan table
+                    cmd.CommandText = "UPDATE Keuangan SET Jumlah = @Jumlah, Keterangan = @Keterangan WHERE ID_Organisasi = @ID_Organisasi";
+                    cmd.Parameters.AddWithValue("@Jumlah", jumlah);
+                    cmd.Parameters.AddWithValue("@Keterangan", keterangan);
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    lblMessage.Text = "Data berhasil diubah.";
+                    LoadJoinedData();
+                }
+                catch (Exception ex)
+                {
+                    transaction?.Rollback();
+                    lblMessage.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            selectedIdOrganisasi = -1;
+            txtNamaOrganisasi.Clear();
+            txtDeskripsi.Clear();
+            txtTahunBerdiri.Clear();
+            txtJumlah.Clear();
+            txtKeterangan.Clear();
+            lblMessage.Text = "";
+            LoadJoinedData();
+        }
+    }
+}
